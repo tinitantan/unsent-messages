a17aa54b04b32ac662294c316e0fa4dc865440db
tinitantan <tanilee44@gmail.com>
2025-06-23 21:50:06 -0400

    hi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 export default function Home() {
-    return (
-      <div style={{ padding: '2rem', textAlign: 'center' }}>
-        <h1>THE LOST LETTERBOX</h1>
-        <p>
-          Inspired by a little Christmas Candy Grams tradition we started, secret messages from loved ones!
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'Arial, sans-serif', color: '#201e1c' }}>
+      <h1>THE LOST LETTERBOX</h1>
+      <p>
+        Inspired by a little Christmas Candy Grams tradition we started, secret messages from loved ones!
+      </p>
+    </div>
+  );
+}
